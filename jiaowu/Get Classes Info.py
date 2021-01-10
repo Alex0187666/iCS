d28b8629300b841a03c9393afbc6cc63a75467ab
White <2063546307@qq.com>
2021-01-10 19:18:31 +0800

    更新

--- jiaowu/Get Classes Info.py
+++ jiaowu/Get Classes Info.py
@@ -20,7 +20,7 @@ class ClassesInfo():
                   '计划人数', '已选人数', '余容', '优选专业', '锁定', '校区', '课程类别', '课程体系', '排课类别', '单列实验']])
 
     def GetInfoUrl(self,url='http://jiaowu.sicau.edu.cn/xuesheng/gongxuan/gongxuan/kai.asp?title_id1=2'):
-        a = requests.get(url, headers={'Connection': 'keep-alive', 'Cache-Control': 'max-age=0', 'Upgrade-Insecure-Requests': '1', 'Origin': 'http://jiaowu.sicau.edu.cn', 'Content-Type': 'application/x-www-form-urlencoded', 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36', 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9', 'Referer': 'http://jiaowu.sicau.edu.cn/web/web/web/index.asp', 'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8', 'Cookie': 'jcrj%5Fbanhao=%E7%94%9F%E5%B7%A5201903;jcrj%5Fnj=2019;jcrj%5Fpwd=275358;jcrj%5Fsession=xt%5Ffb%2Cxt%5Fxxq%2Csf%2Cxueqi%2Csf%5Fpj%2Ctemp%2Cuser%2Cpwd%2Cxm%2Cnj%2Cbanhao%2Cxibie%2Czy%2Cxzy%2C;jcrj%5Fsf=%E5%AD%A6%E7%94%9F;jcrj%5Fsf%5Fpj=%E5%90%A6;jcrj%5Ftemp=6915399432;jcrj%5Fuser=201900550;jcrj%5Fxibie=%E7%94%9F%E5%91%BD%E7%A7%91%E5%AD%A6%E5%AD%A6%E9%99%A2;jcrj%5Fxm=%E7%8E%8B%E8%99%B9%E6%9D%B0;jcrj%5Fxt%5Ffb=%E9%9B%85%E5%AE%89;jcrj%5Fxt%5Fxxq=%E9%9B%85%E5%AE%89;jcrj%5Fxueqi=2020%2D2021%2D1;jcrj%5Fxzy=%E8%AE%A1%E7%AE%97%E6%9C%BA%E7%A7%91%E5%AD%A6%E4%B8%8E%E6%8A%80%E6%9C%AF;jcrj%5Fzy=%E7%94%9F%E7%89%A9%E5%B7%A5%E7%A8%8B;'})
+        a = requests.get(url, headers={'Connection': 'keep-alive', 'Cache-Control': 'max-age=0', 'Upgrade-Insecure-Requests': '1', 'Origin': 'http://jiaowu.sicau.edu.cn', 'Content-Type': 'application/x-www-form-urlencoded', 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36', 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9', 'Referer': 'http://jiaowu.sicau.edu.cn/web/web/web/index.asp', 'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8', 'Cookie': '_ga=GA1.3.412538794.1597571429; ASPSESSIONIDASSSSDDB=JPPBNHEAEFPGDMGNDLLGDAAJ; jcrj%5Fuser=201900550; jcrj%5Fpwd=275358; jcrj%5Fauth=True; jcrj%5Fnj=2019; jcrj%5Fjwc%5Fcheck=y; ASPSESSIONIDAQQTTABD=KLGMIAPAIBMNJJFIKCJABNND; jcrj%5Ftemp=4470636248; jcrj%5Fxm=%CD%F5%BA%E7%BD%DC; jcrj%5Fxt%5Ffb=%D1%C5%B0%B2; jcrj%5Fxt%5Fxxq=%D1%C5%B0%B2; jcrj%5Fsf%5Fpj=%B7%F1; jcrj%5Fxibie=%C9%FA%C3%FC%BF%C6%D1%A7%D1%A7%D4%BA; jcrj%5Fzy=%C9%FA%CE%EF%B9%A4%B3%CC; jcrj%5Ftymfg=%C0%B6%C9%AB%BA%A3%CC%B2; jcrj%5Fxzy=%BC%C6%CB%E3%BB%FA%BF%C6%D1%A7%D3%EB%BC%BC%CA%F5; jcrj%5Fbanhao=%C9%FA%B9%A4201903; jcrj%5Fsf=%D1%A7%C9%FA; jcrj%5Fjc24=%CE%DE; jcrj%5Fjc23=%CE%DE; jcrj%5Fjc52=%CE%DE; jcrj%5Fjc64=%CE%DE; jcrj%5Fjc63=%CE%DE; jcrj%5Fjc62=%CE%DE; jcrj%5Fjc61=%CE%DE; jcrj%5Fjc14=%CE%DE; jcrj%5Fjc41=%CE%DE; jcrj%5Fsession=jwc%5Fcheck%2Cauth%2Cuser%2Cpwd%2Cxt%5Ffb%2Cxt%5Fxxq%2Csf%2Cxueqi%2Csf%5Fpj%2Ctemp%2Cxm%2Cnj%2Cbanhao%2Cxibie%2Czy%2Cxzy%2Cjwc%5Fcheck%2Cxqdz%2Ctymfg%2Cxk%5Fzy%2Cxk%5Fcongxiu%2Cjc11%2Cjc12%2Cjc13%2Cjc14%2Cjc15%2Cjc21%2Cjc22%2Cjc23%2Cjc24%2Cjc25%2Cjc31%2Cjc32%2Cjc33%2Cjc34%2Cjc35%2Cjc41%2Cjc42%2Cjc43%2Cjc44%2Cjc45%2Cjc51%2Cjc52%2Cjc53%2Cjc54%2Cjc55%2Cjc61%2Cjc62%2Cjc63%2Cjc64%2Cjc65%2Cww%5Fzd%2C; jcrj%5Fww%5Fzd=%B1%E0%BA%C5; jcrj%5Fjc21=111111111111111111000000000000; jcrj%5Fjc22=000111111111111000000000000000; jcrj%5Fjc25=000000001111111110000000000000; jcrj%5Fjc51=111111111111111100000000000000; jcrj%5Fjc53=%CE%DE; jcrj%5Fjc54=000001111111111110000000000000; jcrj%5Fjc55=%CE%DE; jcrj%5Fjc31=%CE%DE; jcrj%5Fjc32=010101010101010100000000000000; jcrj%5Fjc33=%CE%DE; jcrj%5Fjc34=001010101010101000000000000000; jcrj%5Fjc35=%CE%DE; jcrj%5Fjc11=111111111111111100000000000000; jcrj%5Fjc12=011111100000000000000000000000; jcrj%5Fjc13=111111111111111000000000000000; jcrj%5Fjc15=000000000001010100000000000000; jcrj%5Fjc65=%CE%DE; jcrj%5Fjc42=111111111111110000000000000000; jcrj%5Fjc43=011111111111111000000000000000; jcrj%5Fjc44=000101010101010000000000000000; jcrj%5Fjc45=111111111111111000000000000000; jcrj%5Fxk%5Fzy=%B7%F1; jcrj%5Fxk%5Fcongxiu=%CA%C7; jcrj%5Fxqdz=http%3A%2F%2Fjiaowu%2Esicau%2Eedu%2Ecn%2Fxuesheng%2Fgongxuan%2Fgongxuan%2Fkai%2Easp%3Ftitle%5Fid1%3D2; jcrj%5Fxueqi=2020%2D2021%2D2'})
         content = a.content.decode('gb2312', 'ignore')
         html = etree.HTML(content)
         names = html.xpath('//td[@width="549"]/a/text()')
@@ -48,10 +48,15 @@ class ClassesInfo():
         driver.find_element_by_id('Userpwd').send_keys('275358')
         driver.find_element_by_xpath('//input[@value="S"]').click()
         driver.find_element_by_name('submit').click()
+        driver.get(url='http://jiaowu.sicau.edu.cn/jiaoshi/bangong/main/xqzt.asp?xueqi=2020-2021-2')
+
+        alert = driver.switch_to.alert  # 创建弹窗对象
+        alert.accept()  # 点击弹窗中的【确定】
 
         for i,j in zip(self.names,self.urls):
+            if i != '全校课程汇总':
+                continue
             print(f'正在收集{i}信息:',j)
-
             '''webdriver'''
             driver.get(j)
 
@@ -81,12 +86,13 @@ class ClassesInfo():
             else:
                 string = '-'
             db.append(string)
-        urls = html.xpath('//a[@onclick="return confirm(\'确定要选择此课程嘛？\')"]/@href')
+        bianhao = html.xpath('''//a[@onclick="return confirm('确定要选择此课程嘛？')"]/@href''')
+        # print(bianhao)
         Info = []
         for i in range(0, len(db), 24):
             Info.append(db[i:i + 24])
         # 组合信息
-        for i, j in zip(Info, urls):
+        for i, j in zip(Info, bianhao):
             j = re.findall('\d{10,}', j)[0]
             i.insert(0, j)
         print(Info)
--- jiaowu/jiaowu.py
+++ jiaowu/jiaowu.py
@@ -1,16 +1,53 @@
-import requests, re
-from lxml import etree
+import requests, re, getpass, csv
 
-import getpass
+from lxml import etree
+from urllib import parse
+
 
 
 class Sicau():
-    '''登陆'''
 
-    def __init__(self, username='', password=''):
+    def __init__(self):
         global isLogin
         self.Requests = requests.session()
+        '''首次运行创建文件'''
+        try:
+            with open('config.txt','r') as f:
+                ck = f.read().replace(' ','').replace('\n','')
+        except:
+            print('首次运行,正在创建配置文件...')
+            with open('config.txt', 'a') as f:
+                f.write(' ')
+            ck = ''
+        if ck:
+            self.ck = ck
+            print('正在加载配置文件...')
+            #检测配置信息是否可用
+            if '2020%2D2021%2D1' in self.ck:
+                print('\r当前学期为:2020-2021-1',end='\r')
+                print('\r已更改学期为:2020-2021-2',end='\r')
+                self.ck.replace('2020%2D2021%2D1', '2020%2D2021%2D2')
+            self.Cktest()
+        else:
+            print('配置文件不可用,请登陆')
+            username = input("请输入学号:")
+            password = getpass.getpass("请输入密码:")
+            self.Login(username=username,password=password)
+        f = csv.reader(open('教务处开课信息(含课程ID）.csv', encoding='utf-8'))
+        data = list(f)
+        self.info = data
+        self.bh_id,self.name_id = {},{}
+        for i in data[1:]:
+            self.bh_id[i[1]] = i[0]
+            self.name_id[i[1]] = str(i[3]) + '-' + str(i[4])
+
+
+    '''登陆'''
+    def Login(self,username,password):
+        global isLogin,SemesterNum
+
         print('\r正在尝试登陆...', end='\r')
+
         '''获取登陆参数sign'''
         a = self.Requests.get('http://jiaowu.sicau.edu.cn/web/web/web/index.asp')
         sign = etree.HTML(a.text).xpath('//input[@name="sign"]/@value')[0]
@@ -18,36 +55,67 @@ class Sicau():
 
         '''进行登陆'''
         url = "http://jiaowu.sicau.edu.cn/jiaoshi/bangong/check.asp"
-
         payload = 'user=%s&pwd=%s&lb=S&submit=&sign=%s' % (username, password, sign)
-        headers = {
-            'Connection': 'keep-alive',
-            'Cache-Control': 'max-age=0',
-            'Upgrade-Insecure-Requests': '1',
-            'Origin': 'http://jiaowu.sicau.edu.cn',
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'Referer': 'http://jiaowu.sicau.edu.cn/web/web/web/index.asp',
-            'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
-        }
+        response = self.Requests.request("POST", url, headers={'Content-Type': 'application/x-www-form-urlencoded'}, data=payload)
 
-        response = self.Requests.request("POST", url, headers=headers, data=payload)
-
+        '''检测登陆状态'''
         ck = ''
         for i, j in response.cookies.items():
             db = i + '=' + j + ';'
             ck += db
         self.ck = ck
         if len(self.ck) <= 70:
-            print("\r登陆失败,用户名或密码错误，请重新尝试\n", end='\r')
+            print("登陆失败,用户名或密码错误，请重新尝试\n")
         else:
             print('\r登陆成功...', end='\r')
+            if self.Requests.cookies.get('jcrj%5Fxueqi') != '2020%2D2021%2D2':
+                print('\r检测当前学期不为:2020-2021-1',end='\r')
+                self.Requests.cookies['jcrj%5Fxueqi'] = '2020%2D2021%2D2'
+                print('\r已更改学期为:2020-2021-2    ',end='\r')
+                SemesterNum = '当前学期:2020-2021-2'
+                self.IntoConfig(self.ck.replace('2020%2D2021%2D1','2020%2D2021%2D2'))
+            print('\r已写入配置文件config.txt中       ',end='\r')
             isLogin = True
-            self.semesterTest()
+            print(self.Requests.cookies.items())
+            # '''学期检测'''
+            # self.semesterTest()
+
+
+    '''写入登陆ck'''
+    def IntoConfig(self,content):
+        with open('config.txt','w') as f:
+            f.write(str(content))
+
+
+    '''检测Ck是否可用'''
+    def Cktest(self):
+        headers = {
+            'Proxy-Connection': 'keep-alive',
+            'Cache-Control': 'max-age=0',
+            'Upgrade-Insecure-Requests': '1',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'Referer': 'http://jiaowu.sicau.edu.cn/xuesheng/gongxuan/gongxuan/xszhinan.asp?title_id1=9&xueqi=2020-2021-2',
+            'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+            'Cookie': self.ck
+        }
+        a = self.Requests.get(url='http://jiaowu.sicau.edu.cn/jiaoshi/bangong/main/xqzt.asp?dizhi=/jiaoshi/bangong/main/welcome1.asp',headers=headers)
+        content = a.content
+        respond = content.decode('gb2312', 'ignore')
+
+        try:
+            global SemesterNum,isLogin
+            SemesterNum = etree.HTML(respond).xpath('//a[@class="menu"]/text()')[0]
+            SemesterNum = SemesterNum[:-5]
+            isLogin = True
+        except:
+            print('配置信息不可用,请尝试登陆')
+            username = input("请输入学号:")
+            password = getpass.getpass("请输入密码:")
+            self.Login(username=username, password=password)
+
 
     '''检测学期信息'''
-
     def semesterTest(self):
         url = 'http://jiaowu.sicau.edu.cn/jiaoshi/bangong/main/xqzt.asp?dizhi=/jiaoshi/bangong/main/welcome1.asp'
         a = self.Requests.get(url)
@@ -60,8 +128,8 @@ class Sicau():
             print('\r检测到当前学期状态不为2020-2021-2,正在尝试更改学期...', end='\r')
             self.ChangeSemester()
 
-    '''更改学期状态'''
 
+    '''更改学期状态'''
     def ChangeSemester(self):
         url = 'http://jiaowu.sicau.edu.cn/jiaoshi/bangong/main/xqzt.asp?xueqi=2020-2021-2&dizhi=/jiaoshi/bangong/main/welcome1.asp'
         a = self.Requests.get(url)
@@ -70,8 +138,8 @@ class Sicau():
         print('\r返回消息:"%s"' % (re.findall("language=javascript>alert\(\'(.*?)\'\);", respond)[0]), end='\r')
         self.semesterTest()
 
-    '''获取预算课程'''
 
+    '''获取预选课程'''
     def GetPreClasses(self):
         url = 'http://jiaowu.sicau.edu.cn/xuesheng/gongxuan/gongxuan/xuan_2018.asp?bianhao=' + str(
             899679718199025481334403813310638134631381992934860344979721228986024077)
@@ -80,38 +148,66 @@ class Sicau():
         respond = content.decode('gb2312', 'ignore')
         print('\r返回消息:', re.findall("<script language=JavaScript>alert\('(.*?)'\);", respond)[0], end='\r')
 
+
     '''选课'''
-
     def ConfireClass(self, ClassID):
-
         '''查取课程编号对应的bianhao'''
-        data = {'test': 'test'}
-        bianhao = data.get(ClassID, None)
+        bianhao = self.bh_id.get(ClassID, None)
         if bianhao:
+            print('\n正在尝试选取%s...',end='\r')
             url = 'http://jiaowu.sicau.edu.cn/xuesheng/gongxuan/gongxuan/xuan_2018.asp?bianhao=' + str(bianhao)
             a = self.Requests.get(url)
             respond = a.content.decode('gb2312', 'ignore')
+            print('\r                           ',end='\r')
             print('\r返回消息:', re.findall("<script language=JavaScript>alert\('(.*?)'\);", respond)[0], end='\r')
         else:
             print('\r输入课程编号有误，请核对后重试:)', end='\r')
 
-    '''获取课程对应的biaohao'''
 
-    def FindUpBiaohao(self, ClassID):
-        data = {'test': 'test'}
-        StuID = data.get(ClassID, '\r输入课程编号有误，请核对后重试:)')
-        '''输出对应课程信息'''
-        print(StuID, end='\r')
+    '''获取课程编号对应信息'''
+    def FindUpBiaohao(self, ClassIDs=[]):
+        num = 0
+        for ClassID in ClassIDs:
+            if ClassID in self.name_id.keys():
+                for i in self.info:
+                    if str(i[1]) == ClassID:
+                        num += 1
+                        print('''序号:%s
+         课程编号:   %s
+         课程名称:   (%s)%s
+         任课单位:   %s
+         任课教师:   %s
+         上课时间:   %s
+         教室地点:   %s
+    计划-已选-余容:   %s
+         优选专业:   %s'''%(num,i[1],i[5],i[4],i[2],i[14],i[7],i[6],str(i[15])+'-'+str(i[16])+'-'+str(i[17]),i[18]))
+            else:
+                print('\r输入课程编号有误，请核对后重试:)', end='\r')
+        if num:
+            print('\n共%s个搜索结果'%(num))
+
+
+
 
     '''抢预选课'''
-
     def ConfirePre(self):
         pass
 
     '''获取本地的课程信息'''
+    def FindByKeywords(self,ClassName):
+        if ClassName in self.name_id.keys():
+            print('有结果')
+        else:
+            db,numData = [],[]
+            for i in ClassName:
+                db.append(i)
+            for i,j in self.name_id.items():
+                # print('nameList:',j)
+                if set(db).issubset(j):
+                    numData.append(i)
+        self.FindUpBiaohao(numData)
 
-    def GetLocalInfo(self):
-        pass
+
 
 
 if __name__ == '__main__':
@@ -133,31 +229,33 @@ if __name__ == '__main__':
                                  CCCC          SSSSSS      
                                        
                             预祝您考试顺利！   
-            (本工具仅作为辅助作用，后果自担，使用前请详细查看教程）                    
+            (本工具仅作为辅助作用，后果自担，使用前请详细查看教程）  
+              GitHub地址:https://github.com/Upsetin/iCS 
+                               
 '''
-    print(Info, end='\r')
+    print(Info)
     SemesterNum = '-'
     isLogin = False
     while not isLogin:
-        username = input("请输入学号:")
-        password = getpass.getpass("请输入密码:")
-        iCS = Sicau(username=username, password=password)
+        iCS = Sicau()
     Judge = True
     while Judge:
         while True:
             try:
                 # print('{:*^50}'.format('请输入对应操作指令:)'),'\n*','{:^50}'.format('1:查看预选课'),'*','\n*','{:^48}'.format('2:抢预选课'),'*','\n*','{:^50}'.format('3:抢指定课程编号课'),'*','\n*','{:^50}'.format('4:查看课程编号对应课程信息'),'*','\n*','{:^50}'.format('0:退出程序',),'*\n',sep='')
                 print(f'''
-\r
-*******************请输入对应操作指令:)********************
+************************操作指令菜单**********************
 *{SemesterNum}                             * 
+*                                                      *
 *                      1:查看预选课程                    *
 *                      2:抢取预选课程                    *
 *                      3:抢指定编号课                    *
 *                      4:查看课程信息                    *
+*                      5:搜索关键词课程信息               *
 *                      0:立即退出程序                    *
-*******************请输入对应操作指令:)********************
-''', end='\r')
+*                                                      *
+*************************操作指令菜单*********************
+请输入指令数字:''',end='')
                 num = int(input())
                 break
             except:
@@ -170,8 +268,13 @@ if __name__ == '__main__':
         elif num == 2:
             pass
         elif num == 3:
-            ClassID = input("请输入课程ID")
+            ClassID = input("请输入课程ID:")
             iCS.ConfireClass(ClassID)
         elif num == 4:
-            ClassID = input("请输入课程ID")
+            ClassID = input("请输入课程ID:")
             iCS.FindUpBiaohao(ClassID)
+        elif num == 5:
+            ClassName = input("请输入课程名称关键词:")
+            iCS.FindByKeywords(ClassName)
+        else:
+            print('⚠️输入指令有误,请核对后重试!')
